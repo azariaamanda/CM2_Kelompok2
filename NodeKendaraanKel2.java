f8203427b3bb4774813315b8e8567d37b42cf546
nanda ricco <nandaricco2677@gmail.com>
2025-05-29 15:39:03 +0700

    Case Method

---
+++ NodeKendaraanKel2.java
@@ -0,0 +1,9 @@
+public class NodeKendaraanKel2 {
+    Kendaraankel2 data;
+    NodeKendaraanKel2 next;
+
+    NodeKendaraanKel2(Kendaraankel2 data, NodeKendaraanKel2 next) {
+        this.data = data;
+        this.next = null;
+    }
+}
--- TransaksiPengisianKel2.java
+++ TransaksiPengisianKel2.java
@@ -3,11 +3,17 @@ public class TransaksiPengisianKel2 {
     BBMKel2 bbm;
     double liter;
     double totalBayar;
+    NodeKendaraanKel2 head;
+    NodeKendaraanKel2 tail;
 
     TransaksiPengisianKel2(KendaraanKel2 kendaraan, BBMKel2 bbm, double liter) {
         this.kendaraan = kendaraan;
         this.bbm = bbm;
         this.liter = liter;
         this.totalBayar = bbm.hargaPerLiter * liter;
+    }
+
+    boolean isEmpty(){
+        return (head == null);
     }
 }
